02007f7e85063cbab611d1fc407db95ab327260a
mw666 <1553851335@qq.com>
2017-06-06 15:39:02 +0800

    gg

---
+++ app/src/main/java/com/tuofu/lenovo/myp2p/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tuofu.lenovo.myp2p;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
